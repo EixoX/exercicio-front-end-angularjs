64caec3f83f51f84a54cc5e30dd3cdccc314be6e
Thiago Capelo <thiago@kpelo.com.br>
2016-04-11 19:08:55 -0300

    Panel Body equalHeight

--- scripts/all.js
+++ scripts/all.js
@@ -1,8 +1,53 @@
 (function () {
     'use strict';
     
-    jQuery('#toggle-sidebar').click(function () {
-        jQuery('.sidebar').toggleClass( 'sidebarPushRight');
-    })
+    var toggleSidebar = function () {
+        jQuery('#toggle-sidebar').click(function () {
+            jQuery('.sidebar').toggleClass( 'sidebarPushRight');
+        })
+    }
+    
+    var equalHeight = function (container) {
+        
+        var currentTallest = 0,
+            currentRowStart = 0,
+            rowDivs = [],
+            topPosition = 0,
+            el = "";
+        
+        jQuery(container).each(function () {
+            el = jQuery(this);
+            jQuery(el).height('auto');
+            topPosition = el.position().top;
+            
+            if(currentRowStart !== topPosition) {
+                for(var currentDiv = 0; currentDiv < rowDivs.length; currentDiv ++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0;
+                currentRowStart = topPosition;
+                currentTallest = el.height();
+                rowDivs.push(el);
+            } else {
+                rowDivs.push(el);
+                currentTallest = (currentTallest < el.height()) ? (el.height()) : (currentTallest);
+            }
+            
+            for (var currentDiv = 0; currentDiv < rowDivs.length; currentDiv ++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
+    
+    
+    
+    jQuery(window).load(function() {
+        toggleSidebar();
+        equalHeight('.panel-body');
+    });
+    
+    jQuery(window).resize(function() {
+        equalHeight('.panel-body');
+    });
     
 })();
