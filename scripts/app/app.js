417a0306f526bc79513f1af7bb3b621578ae9ef9
jeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2017-03-06 18:56:07 -0300

    adminLTE adicionado ao projeto

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -1,6 +1,18 @@
 'use strict';
 
 var glamboxApp = angular.module('glamboxApp', ['ngRoute']);
+
+/****** DIRECTIVES *******/
+
+//Fixlayout
+glamboxApp.directive('fixlayout', function ($timeout) {
+    return function () {
+        function init(){
+        	$.AdminLTE.layout.fix();
+        }
+        $timeout(init(), 0);
+    }
+});
 glamboxApp.config(function($routeProvider){
 	$routeProvider
 		.when('/',
--- scripts/app/app.module.js
+++ scripts/app/app.module.js
@@ -1,3 +1,15 @@
 'use strict';
 
 var glamboxApp = angular.module('glamboxApp', ['ngRoute']);
+
+/****** DIRECTIVES *******/
+
+//Fixlayout
+glamboxApp.directive('fixlayout', function ($timeout) {
+    return function () {
+        function init(){
+        	$.AdminLTE.layout.fix();
+        }
+        $timeout(init(), 0);
+    }
+});
